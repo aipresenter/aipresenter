ac040e0fd91c5d03d1c4a043cbb6226d98a4e52e
Ephraim Acquah <ephraimacquah14@gamail.com>
2023-06-05 14:42:13 -0400

    made reader file to extract from yaml file + test

--- sample/reader.py
+++ sample/reader.py
@@ -8,17 +8,39 @@ class Actor:
         self.age = data['age']
         self.height = data['height']
         
+class Scene:
+    def __init__(self, data):
+        self.name = data['name']
+        self.location = data['location']
+        self.actors = data['actors']
+        self.dialogue = data['dialogue']
+        
+
+class Location:
+    def __init__(self, data):
+        self.name = data['name']
+        self.type = data['type']
+        self.lighting = data['lighting']
+        
 class Reader:
     
     def __init__(self, file_path):
         self.file_path = file_path
         self.data = {}
         self.actors = {}
+        self.scenes = {}
+        self.locations = {}
         with open(self.file_path, 'r') as file:
             self.data = yaml.safe_load(file)
         for actor in self.data['actors']:
             a = Actor(actor)
             self.actors[a.name] = a
+        for scene in self.data['scenes']:
+            s = Scene(scene)
+            self.scenes[s.name] = s
+        for location in self.data['locations']:
+            l = Location(location)
+            self.locations[l.name] = l
             
         
     def get_actors(self):
@@ -26,11 +48,11 @@ class Reader:
         
     
     def get_scenes(self):
-        return self._get_data_element('scenes') 
+        return self.scenes
     
     
     def get_locations(self):
-        return self._get_data_element('locations') 
+        return self.locations 
         
     
     def _get_data_element(self, element):
