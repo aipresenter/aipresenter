a0c71226487c5be9aa67f48efe82455c1fabb054
Ephraim Acquah <ephraimacquah14@gamail.com>
2023-06-08 11:37:36 -0400

    cleaned up linting errors on test_reader

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,6 +1,7 @@
 import unittest
 from ai_presenter.reader import Reader
 
+
 class TestReader(unittest.TestCase):
     def testReader(self):
         x = Reader('tests/text.yml')
@@ -11,19 +12,19 @@ class TestReader(unittest.TestCase):
 
         for actor in ['John Doe', 'Jane Smith', 'Michael Johnson']:
             self.assertIn(actor, a)
-        
+
         for scene in ['Crime Scene', 'Interrogation Room', 'Laboratory']:
             self.assertIn(scene, s)
-            
+
         for location in ['Mountains', 'Cabin']:
             self.assertIn(location, loc)
 
         self.assertDictEqual(a, db.actors)
         self.assertDictEqual(s, db.scenes)
         self.assertDictEqual(loc, db.locations)
-    
 
-            
-            
+
+
+
 if __name__ == '__main__':
     unittest.main()
