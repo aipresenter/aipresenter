756269eb15afd9a3db3a441bf71c5df8effccd09
Ephraim Acquah <ephraimacquah14@gamail.com>
2023-06-08 11:32:38 -0400

    finally fixed import issues

--- ai_presenter/__init__.py
+++
@@ -1,4 +0,0 @@
-##from ai_presenter.ai_presenter import AIPresenter, Generators, 
-# TextAi, TextFake, VoiceAI, VoiceAIFake, ImageAI, ImageAIFake 
-##from reader import Reader
-##from database import Database
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-import ai_presenter
+from ai_presenter.ai_presenter import AIPresenter
+from ai_presenter.reader import Reader
+from ai_presenter.ai_presenter import TextFake
+from ai_presenter.ai_presenter import ImageAIFake
+from ai_presenter.ai_presenter import Generators
+from ai_presenter.ai_presenter import VoiceAIFake
 
 def main():
-    reader = ai_presenter.Reader('sample.yml')
+    reader = Reader('sample.yml')
     db = reader.get_db()
     
-    text_fake = ai_presenter.TextFake()
-    image_fake = ai_presenter.ImageAIFake()
-    voice_fake = ai_presenter.VoiceAIFake()
-    generator = ai_presenter.Generators(text_fake, voice_fake, image_fake)
+    text_fake = TextFake()
+    image_fake = ImageAIFake()
+    voice_fake = VoiceAIFake()
+    generator = Generators(text_fake, voice_fake, image_fake)
     
-    ai = ai_presenter.AIPresenter(db, generator)
+    ai = AIPresenter(db, generator)
     
     ai.run()
 
