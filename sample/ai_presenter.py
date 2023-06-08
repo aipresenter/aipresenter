20a4a1fed3f2aa2f4a78171795baa0656dadbc08
Ephraim Acquah <ephraimacquah14@gamail.com>
2023-06-08 10:27:54 -0400

    added generator class

--- sample/ai_presenter.py
+++ sample/ai_presenter.py
@@ -28,3 +28,64 @@ class TextChatGPT(TextAi):
 
     def generate(self):
         pass
+    
+    
+class VoiceAI:
+    def __init__(self, db: Database):
+        self.actors = db.actors
+        self.scenes = db.scenes
+        self.locations = db.locations
+    
+    
+    def generate(self):
+        pass
+    
+    
+class ImageAI:
+    def __init__(self, db: Database):
+        self.actors = db.actors
+        self.scenes = db.scenes
+        self.locations = db.locations
+        
+        
+    def generate(self):
+        pass
+    
+    
+class VoiceAIFake(VoiceAI):
+    def __init__(self):
+        pass
+    
+    
+    def generate(self):
+        pass
+    
+    
+class ImageAIFake(ImageAI):
+    def __init__(self):
+        pass
+    
+    
+    def generate(self):
+        pass    
+    
+    
+    
+class Generator:
+    
+    def __init__(self, t: TextAi, v: VoiceAI, i: ImageAI):
+        self.text_ai = t
+        self.voice_ai = v
+        self.image_ai = i
+    
+    
+    def get_text(self):
+        return self.text_ai.generate()
+    
+    
+    def get_image(self):
+        return self.image_ai.generate()
+    
+    
+    def get_voice(self):
+        return self.voice_ai.generate()
