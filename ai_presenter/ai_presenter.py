8916b156307edbfda985fc7bc220ef6975af7ea3
Ephraim Acquah <ephraimacquah14@gamail.com>
2023-06-13 12:14:13 -0400

    moved generators class to seperate module

--- ai_presenter/ai_presenter.py
+++ ai_presenter/ai_presenter.py
@@ -1,24 +1,6 @@
 from ai_presenter.database import Database
-from ai_presenter.text_ai.textbase import TextAi
-from ai_presenter.voice_ai.voicebase import VoiceAI
-from ai_presenter.image_ai.imagebase import ImageAI
+from ai_presenter.generators import Generators
 import logging
-
-
-class Generators:
-    def __init__(self, t: TextAi, v: VoiceAI, i: ImageAI):
-        self.text_ai = t
-        self.voice_ai = v
-        self.image_ai = i
-
-    def get_text(self) -> TextAi:
-        return self.text_ai
-
-    def get_image(self) -> ImageAI:
-        return self.image_ai
-
-    def get_voice(self) -> VoiceAI:
-        return self.voice_ai
 
 
 class AIPresenter:
