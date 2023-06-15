1a97b5d49da7911ff23d6472ca10b474564d310f
Ephraim Acquah <ephraimacquah14@gamail.com>
2023-06-15 10:40:58 -0400

    fixed lint errors

--- tests/test_voiceai_fake.py
+++ tests/test_voiceai_fake.py
@@ -11,14 +11,14 @@ class TestVoiceAIFake(unittest.TestCase):
         x = Reader('tests/text.yml')
         db = x.get_db()
         c = VoiceConfig()
-        
+
         fake = VoiceAIFake(db)
         fake.generate(input, output, c)
-        
+
         with open(input, 'r') as input_file:
             in_data = input_file.read()
-            
+
         with open(output, 'r') as output_file:
             out_data = output_file.read()
-            
+
         self.assertEqual(out_data, in_data)
