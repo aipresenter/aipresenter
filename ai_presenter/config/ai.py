03ffd76da4c12e9362496349893c12020e6027ad
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-06-19 12:07:44 -0400

    defaults config section added makes filenames vars
    resolved issue #20

--- ai_presenter/config/ai.py
+++ ai_presenter/config/ai.py
@@ -1,6 +1,9 @@
 from ai_presenter.config.chatgpt import ChatGPTConfig
+import ai_presenter.config.defaults
 
 
 class AIConfig:
     def __init__(self, data):
+        ai_presenter.config.defaults.TEXT_AI_FILE = data['text_ai_filename']
+        ai_presenter.config.defaults.VOICE_AI_FILE = data['voice_ai_filename']
         self.chatgptconfig = ChatGPTConfig(data['chatgpt_config'])
