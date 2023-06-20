b3285e72d38fcde2e66dfffca63652a7de1f7b82
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-06-20 10:17:36 -0400

    changed access of filenames to config class

--- ai_presenter/ai_presenter.py
+++ ai_presenter/ai_presenter.py
@@ -1,6 +1,5 @@
 from ai_presenter.database import Database
 from ai_presenter.generators import Generators
-import ai_presenter.config.defaults
 import logging
 from ai_presenter.voice_ai.base import VoiceAI, VoiceConfig
 
@@ -29,7 +28,10 @@ class AIPresenter:
         # sends message to ai in given format
         textai.send(message)
 
-        with open(ai_presenter.config.defaults.TEXT_AI_FILE, 'w') as file:
+        config = self.database.get_config()
+        text_ai_file = config.get_ai_config().get_text_ai_filename()
+        
+        with open(text_ai_file, 'w') as file:
             textai = self.generator.get_text()
             for key, scene in self.database.scenes.items():
                 logging.info(f"******** \nWorking on scene: {scene.name} in " +
