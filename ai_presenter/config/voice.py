217aeb236a281b66e2a9c065eb84760e2a87fa05
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-06-22 18:25:30 -0400

    added fields to voice_config

--- ai_presenter/config/voice.py
+++ ai_presenter/config/voice.py
@@ -2,5 +2,11 @@
 # VoiceConfig can have passed in name, gender, age,
 # accent, and accent_strength and have these initialized
 class VoiceConfig:
-    def __init__(self):
-        pass
+    def __init__(self, name, gender, age, accent,
+                 accent_strength, description):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.accent = accent
+        self.accent_strength = accent_strength
+        self.description = description
