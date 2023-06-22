a183f2bdec738c9442cb468865812e14196954ae
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-06-22 10:09:16 -0400

    added api key access to ai_config class

--- ai_presenter/config/ai.py
+++ ai_presenter/config/ai.py
@@ -1,5 +1,6 @@
 from ai_presenter.config.chatgpt import ChatGPTConfig
 from ai_presenter.config.defaults import TEXT_AI_FILE, VOICE_AI_FILE
+from ai_presenter.config.env_vars import ELEVENLABS_APIKEY, CHATGPT_APIKEY
 
 
 class AIConfig:
@@ -21,3 +22,9 @@ class AIConfig:
 
     def get_voice_ai_filename(self) -> str:
         return self.voice_ai_filename
+    
+    def get_elevenlabs_api_key(self) -> str:
+        return ELEVENLABS_APIKEY
+    
+    def get_chatgpt_api_key(self) -> str:
+        return CHATGPT_APIKEY
