166447d29e91ca5bd2e3276153292791a689a6f0
Ephraim Acquah <103010587+ephraimacquah@users.noreply.github.com>
2023-06-22 09:37:17 -0400

    created voiceaiactor classes with implementation (#31)
    * created voiceaiactor classes with stub methods
    
    closes #29
    
    I added comments in voice_ai/fake.py and config/voice.py that details my thoughts on how the voiceaiactor class would impact further development
    
    * corrected line 7 with proper comment lines
    
    * fixed lint error
    
    * revised voiceaiactor init + voiceaiactorfake says
    
    initialized seperate fields like name, etc, with config rather than just one config field

--- ai_presenter/voice_ai/base.py
+++ ai_presenter/voice_ai/base.py
@@ -3,6 +3,18 @@ from ai_presenter.config.voice import VoiceConfig
 import logging
 
 
+class VoiceAIActor:
+    def __init__(self, config: VoiceConfig):
+        self.name = config.name
+        self.gender = config.gender
+        self.age = config.age
+        self.accent = config.accent
+        self.accent_strength = config.accent_strength
+
+    def says(self, message, emotion, filename):
+        logging.info(f'{self.config.name} says {message} in a {emotion} way')
+
+
 class VoiceAI:
     def __init__(self, db: Database):
         self.actors = db.actors
@@ -11,3 +23,6 @@ class VoiceAI:
 
     def generate(self, input_file, output_file, c: VoiceConfig):
         logging.info("VoiceAI generating")
+
+    def new_actor(self, config):
+        logging.info("New actor created")
