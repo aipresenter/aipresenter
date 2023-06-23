4423845842e4c9873cee5c81d757ea8e18daf24d
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-06-23 10:19:40 -0400

    updated test_ai_presenter for voiceai support

--- tests/test_ai_presenter.py
+++ tests/test_ai_presenter.py
@@ -3,6 +3,7 @@ import os
 from ai_presenter.ai_presenter import AIPresenter
 from ai_presenter.reader import Reader
 from ai_presenter.text_ai.base import TextAi
+from ai_presenter.voice_ai.base import VoiceAI
 from ai_presenter.database import Database
 from ai_presenter.generators import Generators
 
@@ -18,6 +19,18 @@ class TestTextAi(TextAi):
         return self.expected_text
 
 
+class TestVoiceAI(VoiceAI):
+    def __init__(self, db: Database):
+        super().__init__(db)
+
+    def generate(self, input_file, output_file):
+        with open(input_file, 'r') as file:
+            text = file.read()
+
+        with open(output_file, 'w') as file:
+            file.write(text)
+
+
 class TestAiPresenter(unittest.TestCase):
     def testAiPresenter(self):
         # this variable is used to counteract the calls made
@@ -31,7 +44,8 @@ class TestAiPresenter(unittest.TestCase):
         db = reader.get_db()
         expected = 'HELLO WORLD!'
         text_tester = TestTextAi(db, expected)
-        gen = Generators(text_tester, None, None)
+        voice_tester = TestVoiceAI(db)
+        gen = Generators(text_tester, voice_tester, None)
 
         presenter = AIPresenter(db, gen)
         # self.assertFalse(os.path.exists('text_ai.txt'))
@@ -39,6 +53,11 @@ class TestAiPresenter(unittest.TestCase):
         self.assertTrue(os.path.exists('text_ai.txt'))
         self.assertEqual(text_tester.counter, len(scenes) + setup_calls)
         with open('text_ai.txt', 'r') as file:
+            for line in file:
+                self.assertEqual(expected, line.strip())
+
+        self.assertTrue(os.path.exists('text_ai.txt'))
+        with open('voice_ai.txt', 'r') as file:
             for line in file:
                 self.assertEqual(expected, line.strip())
 
