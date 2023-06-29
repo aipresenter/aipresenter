b2fdcb2341d4597faf17e880ca7fe182d58fac38
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-06-29 13:21:31 -0400

    continued work and testing on clear_voices method
    moved method to ai_presenter, to be called in run

--- ai_presenter/ai_presenter.py
+++ ai_presenter/ai_presenter.py
@@ -1,7 +1,10 @@
 # import json
 import logging
+import requests
 from ai_presenter.database import Database
 from ai_presenter.generators import Generators
+from elevenlabs.api import Voices
+from elevenlabs import set_api_key
 # from ai_presenter.voice_ai.base import VoiceConfig
 
 
@@ -31,3 +34,29 @@ class AIPresenter:
         #   chr_db
         voiceai = self.generator.get_voice()
         voiceai.generate(text_ai_file, voice_ai_file)
+
+
+    def clear_voices(self):
+        file = open("test_clear_voices.txt", "w")
+        
+        config = self.database.get_config()
+        key = config.get_ai_config().get_elevenlabs_api_key()
+        
+        set_api_key(key)
+        voices = Voices.from_api()
+        print(voices)
+        for voice in voices:
+            if voice.category != 'premade':
+                id_num = voice.voice_id
+                url = "https://api.elevenlabs.io/v1/voices/" + id_num
+
+                headers = {
+                "Accept": "application/json",
+                "xi-api-key": key
+                }
+
+
+
+                response = requests.delete(url, headers=headers)
+
+                print(response.text, file=file)
