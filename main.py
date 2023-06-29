8e84da712c0324e12e6902544a76515ac591403b
Immanuel Pabon <88465384+immanuelpabon@users.noreply.github.com>
2023-06-29 15:42:33 -0400

    updated main to dictate the yml file as argument (#50)
    * updated main to dictate the yml file as argument
    
    * changed missing yml error message

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import logging
+import argparse
+import sys
 from ai_presenter.ai_presenter import AIPresenter
 from ai_presenter.reader import Reader
 # from ai_presenter.text_ai.fake import TextFake
@@ -5,13 +8,31 @@ from ai_presenter.image_ai.fake import ImageAIFake
 from ai_presenter.ai_presenter import Generators
 from ai_presenter.voice_ai.elevenlabs import ElevenLabs
 from ai_presenter.text_ai.chatgpt import TextChatGPT
-import logging
+
+USAGE = '''
+Examples:
+
+    python3 main.py --script=myscript.yml
+    python3 main.py --help
+
+'''
+parser = argparse.ArgumentParser(description='AI Presenter')
+parser.add_argument(
+    '--script', dest='script', required=True,
+    default='', help='Path to the YAML script which contains ' +
+    'characters and the plot'
+)
+args = parser.parse_args()
+
+if args.script == '':
+    print("Missing script file")
+    sys.exit(1)
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
     logging.info("Program starting")
-    reader = Reader('sample.yml')
+    reader = Reader(args.script)
     db = reader.get_db()
 
     text_fake = TextChatGPT(db)
