41fccc3eb27b774feb2915828fa6c66c7662796a
immanuelpabon <immanuel@chrysalix.org>
2023-07-06 09:45:15 -0400

    removed logging from elevenlabs.py

--- ai_presenter/voice_ai/elevenlabs.py
+++ ai_presenter/voice_ai/elevenlabs.py
@@ -38,7 +38,6 @@ class VoiceAIActorElevenLabs(VoiceAIActor):
 class ElevenLabs(VoiceAI):
     def __init__(self, db: Database):
         super().__init__(db)
-        logging.info(db.get_config().get_ai_config().get_elevenlabs_api_key())
         set_api_key(db.get_config().get_ai_config().get_elevenlabs_api_key())
 
     def new_actor(self, config):
