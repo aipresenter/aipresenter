353ed45ad7967401b54d439331d781a0892f42df
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-07-14 22:30:26 -0400

    fixed rebase to current changes from incoming

--- ai_presenter/text_ai/chatgpt/text.py
+++ ai_presenter/text_ai/chatgpt/text.py
@@ -11,7 +11,7 @@ from ai_presenter.text_ai.chatgpt.chatgpt import ChatGPT
 # add openai.api_key = config.get_ai_config().get_chatgpt_api_key()
 class TextChatGPT(TextAi):
     def __init__(self, db: Database):
-        self.chatgpt = ChatGPT()
+        self.chatgpt = ChatGPT(db)
         self.db = db
         self.messages = [
             {
