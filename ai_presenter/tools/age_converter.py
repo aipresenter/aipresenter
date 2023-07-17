e50dcf48e13900452b0d0aff79a1578a6e2094ff
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-07-17 17:42:01 -0400

    implemented raising exception for invalid age type

--- ai_presenter/tools/age_converter.py
+++ ai_presenter/tools/age_converter.py
@@ -1,6 +1,8 @@
-def age_converter(age) -> str:
+def age_converter(age: int) -> str:
+    if not isinstance(age, int):
+        raise Exception("Age must be a numerical value")
     if age < 0:
-        raise Exception
+        raise Exception("Age must be greater than or equal to 0")
     if age >= 0 and age < 35:
         return 'young'
     elif age >= 35 and age < 50:
