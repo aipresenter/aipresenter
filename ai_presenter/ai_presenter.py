90e06971e7f73d0ea0f3f0fe5469b0bf8473e660
Ephraim Acquah <ephraimacquah14@gmail.com>
2023-07-17 18:32:28 -0400

    fixed lint errors

--- ai_presenter/ai_presenter.py
+++ ai_presenter/ai_presenter.py
@@ -26,10 +26,9 @@ class AIPresenter:
 
         voiceai = self.generator.get_voice()
         voiceai.generate(text_ai_file, voice_ai_file)
-        
+
     def json_run(self, json_file: str):
         config = self.database.get_config()
         voice_ai_file = config.get_ai_config().get_voice_ai_filename()
         voiceai = self.generator.get_voice()
         voiceai.generate(json_file, voice_ai_file)
-        
